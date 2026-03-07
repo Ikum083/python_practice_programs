b41e380ccc667e4c8c87d6809b8ea0d345a06752
USER <eugenemabansag8@gmail.com>
2026-03-08 00:41:50 +0800

    Added for loop to print all numbers in between the 2 given numbers

--- prog10_batch2.py
+++ prog10_batch2.py
@@ -5,3 +5,8 @@ for i in range(2):
     numbers.append(user_num)
 
 numbers.sort()
+
+temp_number = numbers[1]
+for i in range(numbers[1] - numbers[0] - 1):
+    temp_number -= 1
+    print(temp_number, end = ",")
