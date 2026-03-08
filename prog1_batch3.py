e96ce7bfac6794e952b2a89552b8eb9f342be44a
USER <eugenemabansag8@gmail.com>
2026-03-09 00:49:04 +0800

    Added for loop to check for duplicates and print the ones with no duplicates

--- prog1_batch3.py
+++ prog1_batch3.py
@@ -7,5 +7,10 @@ for i in range(10):
     numbers.append(user_input)
 
 # count how many of a number appears in the list
-
+for j in numbers:
+    element_amount = numbers.count(j)
+    if element_amount == 1:
 # print out numbers with no duplicates
+        print(f"Element with no duplicate: {j}")
+    else:
+        continue
