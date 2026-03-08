3b9c7be63edf482955d4229eb15a65c9bad983b7
USER <eugenemabansag8@gmail.com>
2026-03-09 00:52:54 +0800

    Added pseudocode and basic functionalities

---
+++ prog2_batch3.py
@@ -0,0 +1,10 @@
+# list that contains all elements
+numbers = []
+
+# for loop to ask user to input 10 numbers
+for i in range(10):
+    user_input = float(input("Enter a number: "))
+
+# another for loop to check if every element in the list has a duplicate
+
+# condition to remove the other iterations of said element
