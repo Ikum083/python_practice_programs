9b1af7263d82842809bfee6d311368f639726e64
USER <eugenemabansag8@gmail.com>
2026-03-09 01:21:41 +0800

    Setup program, added list, bool variable and while loop to be used

---
+++ prog4_batch3.py
@@ -0,0 +1,11 @@
+# list to contain all numbers
+numbers = []
+
+# variable to control the while loop
+asking_input = True
+# while loop to keep asking user for inputs until it raises an error
+while asking_input:
+    user_input = float(input("Enter a number: "))
+# conditon to check if the user's input is the lowest number
+
+# print lowest number
