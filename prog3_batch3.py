604e4408f4317942c294c2155985e18483c5c6d0
USER <eugenemabansag8@gmail.com>
2026-03-09 01:13:17 +0800

    Added lists to contain all user input

--- prog3_batch3.py
+++ prog3_batch3.py
@@ -1,11 +1,16 @@
 # boolean variable to control the while loop
 asking_user = True
 
+# lists to contain all user input
+numbers = []
+
 # while loop to keep asking user to input 
 while asking_user:
 # exception handle to stop while loop if value error is raised
     try:
         user_input = float(input("Enter a number: "))
+        numbers.append(user_input)
+        print(numbers)
     except ValueError:
         print("That's not a number!")
         asking_user = False
