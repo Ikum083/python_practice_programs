148962f4c78d4682775a3c4c169b7ac49192e6e0
USER <eugenemabansag8@gmail.com>
2026-03-09 00:45:04 +0800

    Added pseudocode and basic functionalities like for loops

---
+++ prog1_batch3.py
@@ -0,0 +1,11 @@
+# list to store all numbers
+numbers = []
+
+# ask user to input 10 numbers
+for i in range(10):
+    user_input = float(input("Enter a number: "))
+    numbers.append(user_input)
+
+# count how many of a number appears in the list
+
+# print out numbers with no duplicates
