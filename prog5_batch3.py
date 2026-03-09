6a629195562d51be58b54534139bf83867850a3d
USER <eugenemabansag8@gmail.com>
2026-03-09 12:02:23 +0800

    Pesudocode to be used

---
+++ prog5_batch3.py
@@ -0,0 +1,8 @@
+# Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
+# variables to be used : sum of all user input, number of all inputs and bool to control the while loop
+
+# while loop to keep asking user to input numbers until failure
+
+# divide the sum of all inputs and number of inputs
+
+# print result
