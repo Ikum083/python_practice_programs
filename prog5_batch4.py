f7d3890597ffc9b01bcfdb236a8d6458f2171c63
USER <eugenemabansag8@gmail.com>
2026-03-09 13:35:40 +0800

    Added all variables, while loop with exception handling to ask for user input until failure

--- prog5_batch4.py
+++ prog5_batch4.py
@@ -1,7 +1,16 @@
 # variables to be used : bool to control the while loop, sum of all inputs and number of inputs
+asking_user = True
+sum_user_input = 0
+input_quantity = 0
 
 # while loops with exception handling to ensure user inputs only integers
-
+while asking_user:
+    try:
+        sum_user_input += float(input("Enter a number: "))
+        input_quantity += 1
+    except ValueError:
+        print("Invalid input!")
+        asking_user = False
 # compute for average
 
 # print average
