765cc52f9c5d6cdfd71909b4fb8d98a781949611
USER <eugenemabansag8@gmail.com>
2026-03-09 12:22:17 +0800

    Added list of all numbers and for loop to ask for user input 10 times

---
+++ prog1_batch4.py
@@ -0,0 +1,10 @@
+# Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
+# list to contain all numbers
+numbers = []
+
+# for loop to ask user for 10 numbers
+for i in range(10):
+    user_input = float(input("Enter a number: "))
+    numbers.append(user_input)
+
+# condition to check for duplicates
