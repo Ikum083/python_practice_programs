dd92b564e6939cfb3c4b6116f83fb52ba11798d7
USER <eugenemabansag8@gmail.com>
2026-03-09 13:32:59 +0800

    Pseudocode

---
+++ prog5_batch4.py
@@ -0,0 +1,7 @@
+# variables to be used : bool to control the while loop, sum of all inputs and number of inputs
+
+# while loops with exception handling to ensure user inputs only integers
+
+# compute for average
+
+# print average
