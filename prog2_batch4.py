5abd99b8614c0c87d66a19976a4340e9a8193397
USER <eugenemabansag8@gmail.com>
2026-03-09 12:43:23 +0800

    Added for loop to check for quantities of an element, variable to store largest number of duplicates, print said number. Still not fixed

--- prog2_batch4.py
+++ prog2_batch4.py
@@ -7,11 +7,25 @@ asking_input = True
 
 # while loop with exception handling to ask for user input
 while asking_input:
-    user_input = float(input("Enter a number: "))
-    numbers.append(user_input)
-
-# for loop to check the count of every element in the list
+    try:
+        user_input = float(input("Enter a number: "))
+        numbers.append(user_input)
+    except ValueError:
+        print("Invalid input!")
+        asking_input = False
 
 # dictionary to store element with their respective count
+element_quantities = {}
+
+# for loop to check the count of every element in the list
+for i in numbers:
+    # store number with their respective quantity in the dicionary
+    element_quantities[f"{i}"] = numbers.count(i)
+    # remove every iterarion of said element to avoid duplication
+    numbers = [x for x in numbers if x != i]
+
+# find the number with the largest amount of duplicates 
+most_duplicated_num = max(element_quantities, key = element_quantities.get)
 
 # print element with the most duplicates based on dicitonary
+print(most_duplicated_num)
