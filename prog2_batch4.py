0874328159d05a267fe21ca4ac6d414219ea9025
USER <eugenemabansag8@gmail.com>
2026-03-09 12:29:33 +0800

    Pseudocode

---
+++ prog2_batch4.py
@@ -0,0 +1,12 @@
+# Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
+# list to contain all numbers
+
+# bool variable to control while loop
+
+# while loop with exception handling to ask for user input
+
+# for loop to check the count of every element in the list
+
+# dictionary to store element with their respective count
+
+# print element with the most duplicates based on dicitonary
