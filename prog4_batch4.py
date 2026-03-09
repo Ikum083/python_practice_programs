c1bf7777cae9e6c4a9f887cd52b2919077dccd34
USER <eugenemabansag8@gmail.com>
2026-03-09 13:25:32 +0800

    Pseudocode

---
+++ prog4_batch4.py
@@ -0,0 +1,10 @@
+# list to contain all user input
+
+
+# variable to control while loop
+
+# while loop with exception handling to ask for user input and terminate while loop when invalid input
+
+# sort list from highest to lowest
+
+# print every element from list
