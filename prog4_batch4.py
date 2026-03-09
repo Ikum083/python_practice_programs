6e7ce8593de171159bc8480d74f2004ef1bd3cbd
USER <eugenemabansag8@gmail.com>
2026-03-09 13:28:14 +0800

    Added all variables, while loop with exception handling to ensure user inputs only numbers and append to list

--- prog4_batch4.py
+++ prog4_batch4.py
@@ -1,9 +1,17 @@
 # list to contain all user input
-
+numbers = []
 
 # variable to control while loop
+asking_user = True
 
 # while loop with exception handling to ask for user input and terminate while loop when invalid input
+while asking_user:
+    try:
+        user_input = float(input("Enter a number: "))
+        numbers.append(user_input)
+    except ValueError:
+        print("Invalid input!")
+        asking_user = False
 
 # sort list from highest to lowest
 
