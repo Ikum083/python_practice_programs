5ac6e7af6788f889dc2bda8e9b3addd23ff3edba
USER <eugenemabansag8@gmail.com>
2026-03-09 12:32:25 +0800

    Added all variables, created the while loop to ask for user input

--- prog2_batch4.py
+++ prog2_batch4.py
@@ -1,9 +1,14 @@
 # Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
 # list to contain all numbers
+numbers = []
 
 # bool variable to control while loop
+asking_input = True
 
 # while loop with exception handling to ask for user input
+while asking_input:
+    user_input = float(input("Enter a number: "))
+    numbers.append(user_input)
 
 # for loop to check the count of every element in the list
 
