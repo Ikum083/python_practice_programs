5e7672fdf80323d02ecf4121f0db36a80186988c
USER <eugenemabansag8@gmail.com>
2026-03-09 11:29:35 +0800

    Added exception handling for invalid inputs

--- prog4_batch3.py
+++ prog4_batch3.py
@@ -1,3 +1,4 @@
+## Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
 # list to contain all numbers
 numbers = []
 
@@ -5,7 +6,11 @@ numbers = []
 asking_input = True
 # while loop to keep asking user for inputs until it raises an error
 while asking_input:
-    user_input = float(input("Enter a number: "))
+    try:
+        user_input = float(input("Enter a number: "))
+    except ValueError:
+        print("Input is invalid!")
+        asking_input = False
 # conditon to check if the user's input is the lowest number
 
 # print lowest number
