39ffa1e033cb1306e258ca757f70cd713a662be1
USER <eugenemabansag8@gmail.com>
2026-03-09 12:05:40 +0800

    Added all variables to be used, while loop and exception handling to ensure user inputs correct value

--- prog5_batch3.py
+++ prog5_batch3.py
@@ -1,7 +1,18 @@
 # Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
 # variables to be used : sum of all user input, number of all inputs and bool to control the while loop
+input_sum = 0
+input_quantity = 0
+asking_input = True
 
 # while loop to keep asking user to input numbers until failure
+while asking_input:
+    # exception handling
+    try:
+        input_sum += float(input("Enter a number: "))
+        input_quantity += 1
+    except ValueError:
+        print("Wrong input!")
+        asking_input = False
 
 # divide the sum of all inputs and number of inputs
 
