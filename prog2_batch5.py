ad7bf999a5d35340b4f2e960237ed38f5239970d
USER <eugenemabansag8@gmail.com>
2026-03-13 00:12:14 +0800

    Added condition to make sure user only inputs numbers from 0 to 1000

--- prog2_batch5.py
+++ prog2_batch5.py
@@ -1,5 +1,9 @@
 # ask user to input a number from 0 to 1000
 user_input = int(input("Enter a number from 0 to 1000: "))
 
-# f print to print the same digit except as a 6 digit number
-print(f"{user_input:06d}")
+# condition to make sure input is within 0 to 1000
+if user_input <= 1000 and user_input >= 0:  
+    # f print to print the same digit except as a 6 digit number
+    print(f"{user_input:06d}")
+else:
+    print("Wrong input!")
