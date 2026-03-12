3d305b5a62bc56e0a36c8266832e6090c11a8ae2
USER <eugenemabansag8@gmail.com>
2026-03-13 00:03:05 +0800

    Added input for adding name. Used .join() to join the two words in the name and remove extra spaces. Prints the new joined name

--- prog1_batch5.py
+++ prog1_batch5.py
@@ -1,5 +1,8 @@
 # have user input multiple empty spaces before their name
+name = str(input("Enter multiple empty spaces before you name: "))
 
 # join the words in the name to one full name
+new_name = " ".join(name.split())
 
 # print output
+print(new_name)
