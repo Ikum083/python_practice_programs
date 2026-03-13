a368c590b8bd2382953d1c113be74890dae51f62
USER <eugenemabansag8@gmail.com>
2026-03-13 08:36:16 +0800

    Added pseudocode

---
+++ prog8_batch5.py
@@ -0,0 +1,4 @@
+# Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
+# ask user to input full name
+
+# print the number of characters in their name
