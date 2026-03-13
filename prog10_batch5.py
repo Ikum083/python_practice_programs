801b19c5cf81aca5b6afc71f885d65bf38b397cd
USER <eugenemabansag8@gmail.com>
2026-03-13 08:59:17 +0800

    Added input to ask for user full name and printf the name in snake case

--- prog10_batch5.py
+++ prog10_batch5.py
@@ -1,4 +1,6 @@
 # Prog10: Create a program that ask the user to input their fullname in incorrect casing. Print the input in snake case.
 # ask user to input their full name
+full_name = str(input("Enter your full name with improper casing: "))
 
 # print full name in snake case using .lower() and .replace()
+print(f"{(full_name.lower()).replace(" ", "_")}")
