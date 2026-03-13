b330f387b647762aa2721913ade8556f41458686
USER <eugenemabansag8@gmail.com>
2026-03-13 08:31:53 +0800

    Added input for putting user statement and printf the number of characters

--- prog7_batch5.py
+++ prog7_batch5.py
@@ -1,4 +1,6 @@
 # Prog07: Create a program that ask the user to input a complete statement. Print the number of words in the input.
 # ask user to input a statement
+user_statement = str(input("Enter a full statement: "))
 
 # printf number of characters in said input
+print(f"{len(user_statement)}")
