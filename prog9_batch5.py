8d986c0059c836a4295672ca7fd2aa86a5a6eafb
USER <eugenemabansag8@gmail.com>
2026-03-13 08:47:30 +0800

    Added input to ask for full name and printf the name in pascal case using replace and title

--- prog9_batch5.py
+++ prog9_batch5.py
@@ -1,4 +1,6 @@
 # Prog09: Create a program that ask the user to input their fullname in incorrect casing. Print the input in pascal case.
 # ask user to input their full name with improper casing
+full_name = str(input("Enter your full name with improper casing: "))
 
-# print full name using .join() and .title() to print in pascal case
+# print full name using .replace() and .title() to print in pascal case
+print(f"{(full_name.title()).replace(' ', '')}")
