ade549af7d5dd32ac8ad19e6fc80e0cca14e8078
USER <eugenemabansag8@gmail.com>
2026-03-13 08:27:14 +0800

    Added input to ask for full name and print name in reverse casing

--- prog6_batch5.py
+++ prog6_batch5.py
@@ -1,4 +1,6 @@
 # Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
 # have user input their full name with improper casing
+full_name = str(input("Enter your full name in improper casing: "))
 
 # print each character in reverse casing
+print(full_name.swapcase())
