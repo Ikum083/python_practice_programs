34a6da429efe268badaee45067432885ef83f6ed
USER <eugenemabansag8@gmail.com>
2026-03-13 08:37:29 +0800

    Added input to ask for full name and printf the number of characters

--- prog8_batch5.py
+++ prog8_batch5.py
@@ -1,4 +1,6 @@
 # Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
 # ask user to input full name
+full_name = str(input("Enter your full name: "))
 
 # print the number of characters in their name
+print(f"{len(full_name)}")
