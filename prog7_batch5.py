710d3a52c5792ec97575f273ea0e119fe2d6b7ce
USER <eugenemabansag8@gmail.com>
2026-03-13 08:28:50 +0800

    Added pseudocode

---
+++ prog7_batch5.py
@@ -0,0 +1,4 @@
+# Prog07: Create a program that ask the user to input a complete statement. Print the number of words in the input.
+# ask user to input a statement
+
+# printf number of characters in said input
