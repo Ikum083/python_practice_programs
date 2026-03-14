683504845102da93ad3bbf88790228dac18b2b43
USER <eugenemabansag8@gmail.com>
2026-03-14 19:31:24 +0800

    Added all variables, function to mimick rjust using while loops and calls function

--- prog6_batch7.py
+++ prog6_batch7.py
@@ -1,12 +1,23 @@
 # Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
 # sample string
+sample_word = "Hello"
 
 # desired length
+desired_length = 10
 
 # character to be used for fill
+char_fill = "_"
 
 # function to mimick rjust()
-## list that contains every character in the list
-## while loop to keep inserting character fill until it meets the desired length
+def begin_adjust(word_used, length, fill):
+    ## list that contains every character in the list
+    characters = [x for x in sample_word]
+    ## while loop to keep inserting character fill until it meets the desired length
+    while len(characters) != desired_length:
+        characters.insert(0, fill)
+    else:
+        ## print result
+        print(*characters, sep = '')
 
-# print result
+# call function
+begin_adjust(sample_word, desired_length, char_fill)
