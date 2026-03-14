2d113b68b21bb6070d24d4da4f4f7a298272c722
USER <eugenemabansag8@gmail.com>
2026-03-14 18:28:41 +0800

    Added word to be used, function to mimick reverse casing by using their ASCII number and calling the function

--- prog8_batch6.py
+++ prog8_batch6.py
@@ -1,8 +1,19 @@
 # Prog08. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
 # string to be used
-
-# list to contain all characters in the string
+sample_word = "HeLlO wOrD"
 
 # function to turn every lower case character to upper case and vice versa
+def reverse_case(word_used):
+    # list to contain all characters in the string
+    character_list = []
+    for i in word_used:
+        if 'a' <= i <= 'z':
+            character_list.append(chr(ord(i) - 32))
+        elif 'A' <= i <= 'Z':
+            character_list.append(chr(ord(i) + 32))
+        else:
+            character_list.append(i)
+    print(*character_list, sep = '')
 
 # call function
+reverse_case(sample_word)
