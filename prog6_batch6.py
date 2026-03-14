2c38ceb7784d297f689938df090b8e4ba8a551e1
USER <eugenemabansag8@gmail.com>
2026-03-14 17:20:38 +0800

    Added pseudocode

---
+++ prog6_batch6.py
@@ -0,0 +1,10 @@
+# Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
+# string variable to be used
+
+# list to contain every character in the string
+
+# character to be used to add in the list
+
+# function to add characters in the list given 
+
+# print new word
