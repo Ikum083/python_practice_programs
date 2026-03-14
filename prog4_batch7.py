fe1c9b667c3a9a5233ce679c7d803cd5f175d486
USER <eugenemabansag8@gmail.com>
2026-03-14 19:19:29 +0800

    Changed pseudocode, added conditon to check if sample word is the same as a lower cased version of said word

--- prog4_batch7.py
+++ prog4_batch7.py
@@ -1,8 +1,12 @@
 # Prog04. islower() check if all characters of the string is on lower case. Create a program that do the same functionality without using islower() function.
 # string to be used
+sample_word = "Hello world"
 
-# list to check every character in the list
+# condition to check if the sample word is lower case
+if sample_word == sample_word.lower():
+    # print result
+    print("All lowered")
+else:
+    print("Not lowered")
 
-# for loop to check if there is any upper case character in the list
 
-# print result
