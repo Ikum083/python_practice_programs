de0d82b0417729eee08fce3f6140a606592e218b
USER <eugenemabansag8@gmail.com>
2026-03-14 18:23:01 +0800

    Added pseudocode

---
+++ prog8_batch6.py
@@ -0,0 +1,8 @@
+# Prog08. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
+# string to be used
+
+# list to contain all characters in the string
+
+# function to turn every lower case character to upper case and vice versa
+
+# call function
