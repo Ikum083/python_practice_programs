80fa128e7eca2e2fff3e7b8f1bc9dbed7ee0d64b
USER <eugenemabansag8@gmail.com>
2026-03-14 19:25:13 +0800

    Added sample word, prefix word, list of seperated words in the sample list and condition that prints true or false if the prefix word is the same as the first word in the sample string

--- prog5_batch7.py
+++ prog5_batch7.py
@@ -1,10 +1,16 @@
 # Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
 # sample string to be used
+sample_word = "Hello word"
 
 # prefix word to be used
+prefix_word = "Hello"
 
 # list that contains the seperated words from the sample list
+words = [x for x in sample_word.split()]
 
 # condition to check if first element from words list is the same as the prefix word
-
-# print result
+if prefix_word == words[0]:
+    # print result
+    print("True")
+else:
+    print("False")
