042fc584d1a37e1dfad88f3d35f70fb4ad29c33d
USER <eugenemabansag8@gmail.com>
2026-03-14 16:41:27 +0800

    Added pseudocode

---
+++ prog3_batch6.py
@@ -0,0 +1,6 @@
+# Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
+# ask user to input their name
+
+# function to turn every character to lower case using their ASCII values
+
+# print the letter
