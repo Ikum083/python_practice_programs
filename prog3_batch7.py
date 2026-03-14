228844f22cacb1aa65d83a36c42baab0ead7ce00
USER <eugenemabansag8@gmail.com>
2026-03-14 19:03:59 +0800

    Added pseudocode

---
+++ prog3_batch7.py
@@ -0,0 +1,6 @@
+# Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
+# string to be used
+
+# function to change ASCII number of every char in the string
+
+# print new word
