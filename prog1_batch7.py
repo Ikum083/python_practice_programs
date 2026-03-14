15c19912381e4efa37b5f44d27ecef39855e82a2
USER <eugenemabansag8@gmail.com>
2026-03-14 18:55:57 +0800

    Added sample word, split them into two in a list since it doesn't count spaces and print list as a single string

--- prog1_batch7.py
+++ prog1_batch7.py
@@ -1,6 +1,9 @@
 # Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
 # sample string with spaces at the end of the string
+sample_word = "Hello World          "
 
-# function to remove the string
+# function to split the string and remove the spaces
+words = sample_word.split()
 
 # call function 
+print(*words, sep = " ")
