0227f6469d9e3a603548670716d08b413413840e
USER <eugenemabansag8@gmail.com>
2026-03-14 18:31:40 +0800

    Added pseudocode

---
+++ prog9_batch6.py
@@ -0,0 +1,8 @@
+# Prog09. capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
+# string to be used
+
+# function to mimick capitalize
+
+# list to be used in the function
+
+# call function
