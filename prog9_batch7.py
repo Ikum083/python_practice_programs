ce25dd50e4136de982695c5b8ec55a0fc491181d
USER <eugenemabansag8@gmail.com>
2026-03-14 19:51:59 +0800

    Added pseudocode

---
+++ prog9_batch7.py
@@ -0,0 +1,15 @@
+# Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
+# string to be used
+sample_string = "Hello World"
+
+# char parameter to be used
+desired_parameter = 'l'
+
+# function to mimick index()
+def find_index(words_used, char_parameter):
+## list that contains every char in string sample
+    characters = [x for x in words_used]
+## for loop to check for every char and breaks once it finds the first iteration of the desired char
+## print result
+
+# call function
