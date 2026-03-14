c2b433e51e6b7ed9eacc7b08b49101832ea67747
USER <eugenemabansag8@gmail.com>
2026-03-14 19:27:44 +0800

    Added pseudocode

---
+++ prog6_batch7.py
@@ -0,0 +1,12 @@
+# Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
+# sample string
+
+# desired length
+
+# character to be used for fill
+
+# function to mimick rjust()
+## list that contains every character in the list
+## while loop to keep inserting character fill until it meets the desired length
+
+# print result
