4e2fc7f9c624a562d0833f890fa4da94e473b81d
USER <eugenemabansag8@gmail.com>
2026-03-14 16:30:42 +0800

    Added input to ask for full name and function to remove the spaces

--- prog1_batch6.py
+++ prog1_batch6.py
@@ -1,6 +1,9 @@
 # Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
 # ask user to input name with many spaces at the beginning
+full_name = str(input("Enter your name with many spaces before it: "))
 
 # function to remove the spaces in the input
+new_name = " ".join(full_name.split())
 
 # print cleaned name
+print(new_name)
