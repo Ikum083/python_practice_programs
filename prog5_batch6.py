374f9260d1f1edbe20274b83316153ff17f8347c
USER <eugenemabansag8@gmail.com>
2026-03-14 17:12:07 +0800

    Testing split method

--- prog5_batch6.py
+++ prog5_batch6.py
@@ -1,12 +1,17 @@
 # Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
 # string to be used
+full_name = "Eugene Mabansag"
 
 # word to be checked
+end_word = "pdf"
 
 # list to store the seperated words
+seperated_word = []
 
 # function to seperate string
+seperated_word = full_name.split()
 
 # condition to check if the last string is the word to be checked
 
 # print result
+print(seperated_word)
