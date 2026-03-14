5fc66cf2291eade352d6de1fd1f92c713c21d4d5
USER <eugenemabansag8@gmail.com>
2026-03-14 19:06:41 +0800

    Added more pseudocode and setup function

--- prog3_batch7.py
+++ prog3_batch7.py
@@ -1,6 +1,12 @@
 # Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
 # string to be used
+sample_word = "Hello World"
 
 # function to change ASCII number of every char in the string
+def all_caps(word_used):
+    # list to contain every character in the sample word
+    characters = [x for x in sample_word]
+    # for loop to check for every character in the sample word
+    ## conditions to capitalize every lower case character
 
 # print new word
