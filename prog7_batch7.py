4249863e600272d26e4a9f3daa19dcda584704a3
USER <eugenemabansag8@gmail.com>
2026-03-14 19:39:39 +0800

    Added needed variables, function to mimick zfill using while loops and then called function

--- prog7_batch7.py
+++ prog7_batch7.py
@@ -1,11 +1,20 @@
 # Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
 # sample integer to be used
+sample_number = 36
 
 # desired length
+desired_length = 6
 
 # function to add zeroes at the beginning of the integer to meet desired length
-    ## list of every digit in the sample integer    
+def zero_fill(numbers_used, length):
+    ## list of every digit in the sample integer  
+    digits = list(map(int, str(numbers_used)))
     ## added while loop to keep adding 0 to meet desired length
-    ## print result
+    while len(digits) != length:
+        digits.insert(0, 0)
+    else:
+        ## print result
+        print(*digits, sep = '')
 
-# call function   
+# call function
+zero_fill(sample_number, desired_length)   
