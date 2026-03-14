1aa7052ff4e3063219ef566c4c7e75b05ed6dfaf
USER <eugenemabansag8@gmail.com>
2026-03-14 19:34:42 +0800

    Added pseudocode

---
+++ prog7_batch7.py
@@ -0,0 +1,11 @@
+# Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
+# sample integer to be used
+
+# desired length
+
+# function to add zeroes at the beginning of the integer to meet desired length
+    ## list of every digit in the sample integer    
+    ## added while loop to keep adding 0 to meet desired length
+    ## print result
+
+# call function   
