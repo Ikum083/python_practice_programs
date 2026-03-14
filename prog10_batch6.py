3d4600806f19522c1e27b38d6c589f018ef7377a
USER <eugenemabansag8@gmail.com>
2026-03-14 18:43:09 +0800

    Added sample word, function that splits the word and capitalizes every word and then called the function

--- prog10_batch6.py
+++ prog10_batch6.py
@@ -1,6 +1,15 @@
 # Prog10. title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
 # string to be used
+sample_word = "hello world"
 
 # function to seperate the string and capitalize each word
+def proper_case(word_used):
+    # list to contain every word
+    word_list = word_used.split()
+    for i in range(len(word_list)):
+        capitalized_word = word_list[i].capitalize()
+        word_list[i] = capitalized_word
+    # print new word
+    print(*word_list, sep = ' ')
 
-# print new word
+proper_case(sample_word)
