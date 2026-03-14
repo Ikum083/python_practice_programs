bb89736e523ebc1a084544b91c8c6fc6ef1d1de7
USER <eugenemabansag8@gmail.com>
2026-03-14 16:59:44 +0800

    Added pseudocode

---
+++ prog4_batch6.py
@@ -0,0 +1,8 @@
+# Prog04. isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
+# input to ask user for their full name
+
+# list to put every character in the new name
+
+# function to upper case every character
+
+# print new name
