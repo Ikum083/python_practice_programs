da6ade86880a061d4e1fbd58dc5e41e8d410ccd0
USER <eugenemabansag8@gmail.com>
2026-03-14 18:53:31 +0800

    Added pseudocode

---
+++ prog1_batch7.py
@@ -0,0 +1,6 @@
+# Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
+# sample string with spaces at the end of the string
+
+# function to remove the string
+
+# call function 
