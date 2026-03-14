cacd65ca92561eb41078afbaf1e6f359c4f17d18
USER <eugenemabansag8@gmail.com>
2026-03-14 19:02:36 +0800

    Added sample string, a list that seperates the words in the given string, remove the 'txt' suffix and printed the new word

--- prog2_batch7.py
+++ prog2_batch7.py
@@ -1,8 +1,12 @@
 # Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
 # string to be used 
+sample_word = "Hello_world.txt"
 
 # list that contains the seperated string
+words = [x for x in sample_word.split('.')]
 
 # remove the suffix
+words.remove('txt')
 
 # print new word with the removed suffix
+print(*words)
