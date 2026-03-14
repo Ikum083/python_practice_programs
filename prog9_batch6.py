3070834c81b259e8193feb6e29e5d15b29fdb7aa
USER <eugenemabansag8@gmail.com>
2026-03-14 18:37:21 +0800

    Added sample word, function to use ASCII number to change character to its capitalize counterpart and call the function

--- prog9_batch6.py
+++ prog9_batch6.py
@@ -1,8 +1,14 @@
 # Prog09. capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
 # string to be used
+sample_word = "hello world"
 
 # function to mimick capitalize
-
-# list to be used in the function
+def capital_string(word_used):
+    # list to be used in the function
+    character_list = [x for x in word_used]
+    if 'a' <= character_list[0] <= 'z':
+        character_list[0] = chr(ord(character_list[0]) - 32)
+    print(*character_list, sep = "") 
 
 # call function
+capital_string(sample_word)
