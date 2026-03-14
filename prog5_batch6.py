68d787a1a0a5a2986a9b7cd817d9ff209ea14f6c
USER <eugenemabansag8@gmail.com>
2026-03-14 17:09:34 +0800

    Added pseudocode

---
+++ prog5_batch6.py
@@ -0,0 +1,12 @@
+# Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
+# string to be used
+
+# word to be checked
+
+# list to store the seperated words
+
+# function to seperate string
+
+# condition to check if the last string is the word to be checked
+
+# print result
