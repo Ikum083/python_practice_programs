12b661ccd1183f6a13ff177b38cc251dfe160b7a
USER <eugenemabansag8@gmail.com>
2026-03-14 19:57:08 +0800

    Added pseudocode

---
+++ prog10_batch7.py
@@ -0,0 +1,13 @@
+# Prog10. rindex() return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
+# string to be used
+
+# char parameter to be used
+
+# function to mimick rindex()
+## list that contains every character in the string
+## reverse list content
+## use for loop to find first iteration of the char parameter
+## return index
+
+# call function
+# print result 
