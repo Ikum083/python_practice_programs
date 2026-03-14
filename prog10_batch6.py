0cbcff166b60703620b5d33dc6be41c8f599db12
USER <eugenemabansag8@gmail.com>
2026-03-14 18:38:37 +0800

    Added pseudocode:

---
+++ prog10_batch6.py
@@ -0,0 +1,6 @@
+# Prog10. title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
+# string to be used
+
+# function to seperate the string and capitalize each word
+
+# print new word
