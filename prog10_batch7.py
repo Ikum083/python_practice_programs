95cc0e1b217196065a7d25d2fee6bce0ed052d62
USER <eugenemabansag8@gmail.com>
2026-03-14 20:06:37 +0800

    Added all variables, added function that reverses the sample string and uses for loop to find last iteration of a letter starting from the last character

--- prog10_batch7.py
+++ prog10_batch7.py
@@ -1,13 +1,26 @@
 # Prog10. rindex() return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
 # string to be used
+sample_word = "Hello World"
 
 # char parameter to be used
+iterated_char = 'l'
 
 # function to mimick rindex()
+def reversed_index(words_used, char_parameter) -> int:
 ## list that contains every character in the string
+    characters = [x for x in words_used]
 ## reverse list content
+    characters.reverse()
 ## use for loop to find first iteration of the char parameter
-## return index
+    for i in range(len(characters)):
+        if characters[i] == char_parameter:
+            ## return index
+            return len(characters) - (i + 1)
+        else:
+            continue
 
 # call function
+iteration_index = reversed_index(sample_word, iterated_char)
+
 # print result 
+print(iteration_index)
