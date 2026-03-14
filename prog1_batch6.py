c05c7c40b000c1b46ebbe6ffbf2056491cb85fae
USER <eugenemabansag8@gmail.com>
2026-03-14 16:28:53 +0800

    Added pseudocode:

---
+++ prog1_batch6.py
@@ -0,0 +1,6 @@
+# Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
+# ask user to input name with many spaces at the beginning
+
+# function to remove the spaces in the input
+
+# print cleaned name
