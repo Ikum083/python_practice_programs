789dd00e1a5151897087c96ee05de7aa185c4a8c
USER <eugenemabansag8@gmail.com>
2026-03-14 19:41:57 +0800

    Added pseudocode

---
+++ prog8_batch7.py
@@ -0,0 +1,11 @@
+# Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
+# sample string to be used
+
+# parameter char to be used
+
+# function to mimick count()
+## list that contains every char in the string
+## while loop to count every iteration of the parameter in the word
+## print result
+
+# call function
