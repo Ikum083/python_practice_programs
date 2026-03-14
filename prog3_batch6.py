9410c0db4f7b43f859e6c6035f6fed4c9410895d
USER <eugenemabansag8@gmail.com>
2026-03-14 16:57:48 +0800

    Added input to ask for user full name, a variable list to put each character in the name, function to lower case every character, print the new name

--- prog3_batch6.py
+++ prog3_batch6.py
@@ -1,6 +1,19 @@
 # Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
 # ask user to input their name
+full_name = str(input("Enter your full name: "))
+
+# list to add every character
+new_name_list = []
+
 
 # function to turn every character to lower case using their ASCII values
+def lower_case_name(full_name, new_name_list):
+    for i in full_name:
+        if 'A' <= i <= 'Z':
+            new_name_list.append(chr(ord(i) + 32))
+        else:
+            new_name_list.append(i)
 
 # print the letter
+lower_case_name(full_name, new_name_list)
+print(*new_name_list, sep='')
