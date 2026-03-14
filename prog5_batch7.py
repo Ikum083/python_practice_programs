66ab7b8e4fa229949fe47c22d87c871275eed384
USER <eugenemabansag8@gmail.com>
2026-03-14 19:22:05 +0800

    Added pseudocode

---
+++ prog5_batch7.py
@@ -0,0 +1,10 @@
+# Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
+# sample string to be used
+
+# prefix word to be used
+
+# list that contains the seperated words from the sample list
+
+# condition to check if first element from words list is the same as the prefix word
+
+# print result
